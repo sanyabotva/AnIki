213e09e75e6be1f19355226d9357c827d30e90e5
sanyabotva <shaneb0tw1n@yandex.ru>
2024-12-30 21:54:23 +0100

    Initial commit

---
+++ AnIkiTests/AnIkiTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnIkiTests.swift
+//  AnIkiTests
+//
+//  Created by Timur Baiazitov on 30.12.2024.
+//
+
+import Testing
+@testable import AnIki
+
+struct AnIkiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
